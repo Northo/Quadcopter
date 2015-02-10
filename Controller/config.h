e768e70afc4a638ee6438c93d82025d207b18362
Thorvald <none>
2015-02-10 18:59:25 +0100

    Bagan controller. Many defines without values, only layout

---
+++ Controller/config.h
@@ -0,0 +1,41 @@
+//----------ESC's config-----------------
+
+//PWM values
+#define MOTOR_ZERO_LEVEL 125
+#define MOTOR_ARM 140
+#define MOTOR_MAX_LEVEL 254
+
+//Motor pins
+#define MOTOR_FR 6
+#define MOTOR_FL 10
+#define MOTOR_BR 5
+#define MOTOR_BL 9
+
+//----------Rx config-------------------
+//------Pins------
+#define RX_PIN_TROTTLE
+#define RX_PIN_PITCH
+#define RX_PIN_ROLL
+#define RX_PIN_YAW
+//Auxillary - button and knobs
+#define RX_PIN_AUX1
+#define RX_PIN_AUX2
+
+//------KEY VALUES-
+#define TROTTLE_MIN 
+#define TROTTLE_MAX
+
+#define PITCH_MIN 
+#define PITCH_MAX
+
+#define ROLL_MIN 
+#define ROLL_MAX
+
+#define YAW_MIN 
+#define YAW_MAX
+
+#define AUX1_MIN
+#define AUX1_MAX
+
+#define AUX2_MIN
+#define AUX2_MAX
