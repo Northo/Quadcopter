5a504628b1afeef5340a182f97af13774c1d84e7
Thorvald <none>
2015-02-24 19:11:00 +0100

    Makes file Controller.ino - the main file

---
+++ Controller/Controller.ino
@@ -0,0 +1,17 @@
+/*
+Karsten Sebastian Stadler, Martin Due Andersen and Thorvald Molthe Ballestad
+ Ullern VGS - 2015
+
+Controller.ino is the main file.
+ */
+#include "config.h"
+#include <PID>
+
+
+void setup() {
+  
+}
+
+void loop() {
+
+}
