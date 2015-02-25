ef12ade57562ed7422e5c7c6ee14e3586a3dbcb5
Northo <thorvald.tb@gmail.com>
2015-02-25 11:45:00 +0100

    calculates yaw roll and pitch very poorly

--- Quadcopter/FlightController.ino
+++ Quadcopter/FlightController.ino
@@ -4,6 +4,11 @@ void FlightController() {
 
   //motor values are computed by adding throttle, roll, yaw and pitch
   throttle = map(rxThrottle, RX_THROTTLE_MIN, RX_THROTTLE_MAX, MOTOR_ZERO_LEVEL, MOTOR_MAX_LEVEL);
+  
+  //For debug and test only
+  pitch = map(rxPitch, RX_PITCH_MIN, RX_PITCH_MAX, -50, 50);
+  roll  = map(rxRoll, RX_ROLL_MIN, RX_ROLL_MAX, -50, 50);
+  yaw   = map(rxYaw, RX_YAW_MIN, RX_YAW_MAX, -50, 50);
 
   motorFrValue = throttle - pitch + roll + yaw;
   motorFlValue = throttle - pitch - roll + yaw;
