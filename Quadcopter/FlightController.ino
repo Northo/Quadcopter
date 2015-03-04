54d0403cadff5d5346120386e0bb3e882e5be1fb
Thorvald <none>
2015-03-04 19:42:04 +0100

    Activates roll and yaw after debug and adds roll, pitch and yaw max/min values

--- Quadcopter/FlightController.ino
+++ Quadcopter/FlightController.ino
@@ -6,20 +6,19 @@ void FlightController() {
   throttle = map(rxThrottle, RX_THROTTLE_MIN, RX_THROTTLE_MAX, MOTOR_ZERO_LEVEL, MOTOR_MAX_LEVEL);
   
   //For debug and test only
-  pitch = map(rxPitch, RX_PITCH_MIN, RX_PITCH_MAX, -50, 50);
-  roll  = map(rxRoll, RX_ROLL_MIN, RX_ROLL_MAX, -50, 50);
-  yaw   = map(rxYaw, RX_YAW_MIN, RX_YAW_MAX, -50, 50);
+  pitch = map(rxPitch, RX_PITCH_MIN, RX_PITCH_MAX, PITCH_MIN, PITCH_MAX);
+  roll  = map(rxRoll, RX_ROLL_MIN, RX_ROLL_MAX, ROLL_MIN, ROLL_MAX);
+  yaw   = map(rxYaw, RX_YAW_MIN, RX_YAW_MAX, YAW_MIN, YAW_MAX);
 
-/*
   motorFrValue = throttle - pitch + roll + yaw;
   motorFlValue = throttle - pitch - roll + yaw;
   motorBrValue = throttle + pitch + roll - yaw;
-  motorBlValue = throttle + pitch - roll - yaw;*/
+  motorBlValue = throttle + pitch - roll - yaw;
 
-  motorFrValue = throttle - pitch;
+  /*  motorFrValue = throttle - pitch;
   motorFlValue = throttle - pitch;
   motorBrValue = throttle + pitch;
-  motorBlValue = throttle + pitch;
+  motorBlValue = throttle + pitch;*/
 
   //PWM might damage motors
   analogWrite(MOTOR_FR, motorFrValue);
