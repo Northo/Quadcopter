8f5b68f05a4242608e7e9808aa59fc798f714feb
Thorvald <none>
2015-04-16 18:20:16 +0200

    adds gyro.ino

---
+++ Quadcopter/Gyro.ino
@@ -0,0 +1,6 @@
+void Gyro() {
+  int i;
+  for(i=0; i<NUMBER_OF_AXIS; i++) {
+    gyro[i]=analogRead(gyroPin[i]-337); //Analog read gir en verdi mellom 0 og 1024 der 0 = 0v og 1024 = 5v. Gyro gir ut 3.3v. 0 akseleration gir ut 1.65v, altså halvparten av 3.3. 337 = (1024/5v)*3.3v/2. Dette kan gjøres mer elegant i fremtiden.
+  }
+}
