29ad4aa891960cb9abd527faed502eed52d8265a
Northo <thorvald.tb@gmail.com>
2015-04-28 15:00:53 +0200

    change pin

--- Quadcopter/FlightController.ino
+++ Quadcopter/FlightController.ino
@@ -5,14 +5,14 @@ void FlightController() {
   //motor values are computed by adding throttle, roll, yaw and pitch
   throttle = map(rxThrottle, RX_THROTTLE_MIN, RX_THROTTLE_MAX, THROTTLE_MIN, THROTTLE_MAX);
   
-  pitch = map(rxPitch, RX_PITCH_MIN, RX_PITCH_MAX, PITCH_MIN, PITCH_MAX);
-  roll  = map(rxRoll, RX_ROLL_MIN, RX_ROLL_MAX, ROLL_MIN, ROLL_MAX);
-  yaw   = map(rxYaw, RX_YAW_MIN, RX_YAW_MAX, YAW_MIN, YAW_MAX);
+  pitch = map(rxPitch, RX_PITCH_MIN, RX_PITCH_MAX, PITCH_MIN, PITCH_MAX)*SENSITIVITY;
+  roll  = map(rxRoll, RX_ROLL_MIN, RX_ROLL_MAX, ROLL_MIN, ROLL_MAX)*SENSITIVITY;
+  yaw   = map(rxYaw, RX_YAW_MIN, RX_YAW_MAX, YAW_MIN, YAW_MAX)*SENSITIVITY;
 
-  motorFrValue = throttle - pitch - roll + yaw;
-  motorFlValue = throttle - pitch + roll - yaw;
-  motorBrValue = throttle + pitch - roll - yaw;
-  motorBlValue = throttle + pitch + roll + yaw;
+  motorFrValue = throttle - pitch + roll - yaw;
+  motorFlValue = throttle - pitch - roll + yaw;
+  motorBrValue = throttle + pitch + roll + yaw;
+  motorBlValue = throttle + pitch - roll - yaw;
 
 #ifdef SAFE
   if(rxAux1 < (RX_AUX1_MAX + RX_AUX1_MIN)/2 ) {
